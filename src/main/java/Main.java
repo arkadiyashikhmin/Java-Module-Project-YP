3e88fb69e7d82f5381ca42c89c88a81832f1c428
Test Ashih <qaashihqa@gmail.com>
2023-10-31 22:53:01 +0300

    Коммит с ДЗ по практической №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
 
 public class Main {
+    public static double sum;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int quantityPerson;
+        quantityPerson = 0;
+        double sum;
+        String messageTemplate = "\nСумма, которую должен заплатить каждый человек %.2f";
+
+
+        // запрашиваем у пользователя данные по количеству человек
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На скольких человек необходимо разделить счёт? ");
+
+        while(true) {
+            if (scanner.hasNextInt()) { // проверяемя введена ли цифра
+                quantityPerson = scanner.nextInt();
+                if (quantityPerson > 1) { // если количество гостей > 1, то выходим из цикла
+                    break;
+                } else {
+                    System.out.print("Ошибка. Пожалуйста, введите верное количество гостей ");
+                }
+                 } else {
+                System.out.print("Вы ввели не число. Пожалуйста, введите число "); // если введено не число
+
+            }
+            scanner.nextLine();
+            }
+
+            Product.calculate();
+
+        sum = Product.totalPrice/quantityPerson;
+
+        System.out.println("Добавленные товары: \n" + Product.nameList);
+        //System.out.println("Количество человек = " + quantityPerson);
+        System.out.println(String.format(messageTemplate,sum) + " рубл" + Rubles.vivod(sum));
+
+
+        }
     }
-}
---
+++ src/main/java/Product.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Product {
+    static String name;
+    static double price;
+
+    static double totalPrice;
+    static String nameList = "";
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void calculate() {
+
+        // Добавляем товар в цикле
+        while (true) {
+            System.out.println("Введите название товара");
+            name = scanner.nextLine();
+            nameList += "\n" + name;
+            System.out.println("Введите цену товара в формате: рубли.копейки");
+            while (true) {
+                if (scanner.hasNextDouble()) {
+                    price = scanner.nextDouble();
+                    if (price > 0) {
+                        totalPrice += price;
+                        System.out.print("Товар успешно добавлен.\n");
+                        break;
+                    } else {
+                        System.out.print("Цена не может быть < = 0. Введите правильную цену ");
+                    }
+                } else {
+                    System.out.print("Ошибка. Введите цену цифрах ");
+                }
+                scanner.nextLine();
+            }
+            scanner.nextLine();
+            price = price + price;
+            AddProduct.question();
+            break;
+        }
+    }
+}
+
